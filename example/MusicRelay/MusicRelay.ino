b57af70515f0edf8bb665646e092c1387b42dd46
Jin-314 <yuujinyuujin77@gmail.com>
2020-10-03 09:00:32 +0900

    COMMIT

--- example/MusicRelay/MusicRelay.ino
+++ example/MusicRelay/MusicRelay.ino
@@ -1,31 +1,33 @@
-int pin[10] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};                     
-char message[5];
+int pin[10] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 String eventType = "";
-
 static int buff[20] = {0};
 int parts[10] = {0};
 int index = 0;
-int counter1 = 0;
-int counter2 = 0;
-int inByte = 0;
-
+int cnt1 = 0;
+int cnt2 = 0;
+char inByte = 0;
+unsigned long pretime[10] = {0};
 void setup() {
   //Serial Initialize  
-  Serial.begin(115200);
+  Serial.begin(2000000);
+  for(int i = 0; i < 10; i++){
+    pinMode(pin[i], OUTPUT);
+  }
 }
 
 void loop() {
   unsigned long mtime = micros();
-  unsigned long pretime = mtime;
-  for(int i = 0; i < 5; i++){
-    if(mtime - pretime < parts[i] / 2){
-      digitalWrite(pin[i], HIGH);
-      digitalWrite(pin[i + 5], HIGH);
-    }else if(mtime - pretime < parts[i]){
-      digitalWrite(pin[i], LOW);
-      digitalWrite(pin[i + 5], LOW);
-    }else{
-      pretime = mtime;
+  char message[5];
+  int partIdx = CountParts();
+  for(int i = 0; i < partIdx; i++){
+    if(parts[i] > 0){
+      if(mtime - pretime[i] < (parts[i] / 2)){
+        digitalWrite(pin[i], HIGH);
+      }else if(mtime - pretime[i] < parts[i]){
+        digitalWrite(pin[i], LOW);
+      }else{
+        pretime[i] = mtime;
+      }      
     }
   }
   while(Serial.available()){
@@ -33,41 +35,37 @@ void loop() {
     inByte = Serial.read();
       
     if(inByte != 44){
-      if(counter2 == 1){
-        message[counter1] = inByte;
+      if(cnt2 == 1){
+        message[cnt1] = inByte;
       }else{
-        buff[counter1] = inByte - '0';
+        buff[cnt1] = inByte - '0';
       }
-      counter1++;
-      
+      cnt1++;
     }else{
-      switch(counter2){
+      switch(cnt2){
         case 0:
           index = decorder();
-          counter2++;
+          cnt2++;
           break;
         case 1:
+          message[cnt1] = '\0';
           eventType = message;
           if(eventType == "OFF"){
-            counter2 = 0;
+            cnt2 = 0;
             parts[index - 1] = 0;
           }else{
-            counter2++;
+            cnt2++;
           }
           break;
         case 2:
           parts[index - 1] = decorder();
-          counter2 = 0;
+          cnt2 = 0;
           break;
       }
-      counter1 = 0;
+      cnt1 = 0;
     }
-    delayMicroseconds(100);
   }
-   counter2 = 0;
-   for(int i = 0; i < 10; i++){
-     parts[i] = 0;
-   }
+   //delayMicroseconds(50);
 }
 
 //decode ascii to number
@@ -75,11 +73,33 @@ int decorder(){
   
   int result = 0;
   
-  for(int i = 0; i < counter1; i++){
-    int a = pow(10, (counter1 - 1) - i);
-    result += buff[i] * a;
+  for(int i = 0; i < cnt1; i++){
+    int exponent = (cnt1 - 1) - i;
+    double tmp = pow(10, exponent);
+    result += buff[i] * tmp;
     buff[i] = 0;
   }
   
   return result;
+}
+
+int CountParts(){
+  int i;
+  for(i = 0; i < 10; i++){
+    if(!parts[i]){
+      if(i < 9){
+        for(int j = 1; j < 10 - i; j++){
+          if(parts[i + j] == 0 && j + 1 == 9 - i){
+            return i;
+          }
+          if(parts[i + j] != 0){
+            break;
+          }
+        }
+      }else{
+        return i;
+      }
+    }
+  }
+  return 0;
 }
