b0f5f8f2009f1da4b8ad6b2fa7d0f37d52d7b6b8
Jin <58265068+Jin-314@users.noreply.github.com>
2020-10-05 20:27:04 +0900

    'ベータ版v1.0'

--- example/MusicRelay/MusicRelay.ino
+++ example/MusicRelay/MusicRelay.ino
@@ -1,12 +1,19 @@
 int pin[10] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 String eventType = "";
 static int buff[20] = {0};
-int parts[10] = {0};
+typedef struct {
+  int period;
+  unsigned long mtime;
+  unsigned long pretime;
+  double duty;
+  bool flug;
+} partData;
+partData parts[10] = {0, 0, 0, 0, false};
 int index = 0;
 int cnt1 = 0;
 int cnt2 = 0;
 char inByte = 0;
-unsigned long pretime[10] = {0};
+
 void setup() {
   //Serial Initialize  
   Serial.begin(2000000);
@@ -16,20 +23,27 @@ void setup() {
 }
 
 void loop() {
-  unsigned long mtime = micros();
   char message[5];
   int partIdx = CountParts();
+  //同時に鳴らすパートの数分繰り返す
   for(int i = 0; i < partIdx; i++){
-    if(parts[i] > 0){
-      if(mtime - pretime[i] < (parts[i] / 2)){
+    if(parts[i].period > 0){
+      parts[i].mtime = micros();
+      //各パートごとに鳴らすか鳴らさないか判定
+      if(!parts[i].flug && 
+            parts[i].mtime - parts[i].pretime < (double)parts[i].period * (parts[i].duty / 100)){
         digitalWrite(pin[i], HIGH);
-      }else if(mtime - pretime[i] < parts[i]){
+        parts[i].flug = true;
+      }else if(parts[i].flug && 
+            parts[i].mtime - parts[i].pretime > (double)parts[i].period * (parts[i].duty / 100)){
         digitalWrite(pin[i], LOW);
-      }else{
-        pretime[i] = mtime;
+        parts[i].flug = false;
+      }else if(parts[i].mtime - parts[i].pretime > parts[i].period){
+        parts[i].pretime = parts[i].mtime;
       }      
     }
   }
+  //シリアルに音階の周期が送られてきたら適宜変更
   while(Serial.available()){
     
     inByte = Serial.read();
@@ -43,29 +57,43 @@ void loop() {
       cnt1++;
     }else{
       switch(cnt2){
-        case 0:
+        case 0:                   //First data is index
           index = decorder();
           cnt2++;
           break;
-        case 1:
+        case 1:                   //The next data is kind of Events
           message[cnt1] = '\0';
           eventType = message;
           if(eventType == "OFF"){
             cnt2 = 0;
-            parts[index - 1] = 0;
+            parts[index - 1].period = 0;
           }else{
             cnt2++;
           }
           break;
-        case 2:
-          parts[index - 1] = decorder();
+        case 2:                   //Final data is period
+          parts[index - 1].period = decorder();
+          //デューティ比設定
+          if(parts[index - 1].period <= 3500 && parts[index - 1].period >= 2000){        
+            parts[index - 1].duty = 100.00 - map(parts[index - 1].period, 2000, 3500, 20, 70);
+          }else if(parts[index - 1].period <= 6000 && parts[index - 1].period > 3500){
+            parts[index - 1].duty = 100.00 - map(parts[index - 1].period, 3500, 6000, 55, 75);
+          }else if(parts[index - 1].period <= 8000 && parts[index - 1].period > 6000){
+            parts[index - 1].duty = 100.00 - map(parts[index - 1].period, 6000, 8000, 70, 80);
+          }else if(parts[index - 1].period <= 15000 && parts[index - 1].period > 8000){
+            parts[index - 1].duty = 100.00 - map(parts[index - 1].period, 8000, 15000, 80, 90);
+          }else{
+            if(parts[index - 1].period > 15000)
+              parts[index - 1].duty = 10;
+            else
+              parts[index - 1].duty = 90;
+          }
           cnt2 = 0;
           break;
       }
       cnt1 = 0;
     }
   }
-   //delayMicroseconds(50);
 }
 
 //decode ascii to number
@@ -86,13 +114,13 @@ int decorder(){
 int CountParts(){
   int i;
   for(i = 0; i < 10; i++){
-    if(!parts[i]){
+    if(!parts[i].period){
       if(i < 9){
         for(int j = 1; j < 10 - i; j++){
-          if(parts[i + j] == 0 && j + 1 == 9 - i){
+          if(parts[i + j].period == 0 && j + 1 == 9 - i){
             return i;
           }
-          if(parts[i + j] != 0){
+          if(parts[i + j].period != 0){
             break;
           }
         }
